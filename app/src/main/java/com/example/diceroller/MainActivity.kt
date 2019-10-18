812b37bd0f52c79d20525f837c650c5d92b32fbf
yspua1999 <56663047+yspua1999@users.noreply.github.com>
2019-10-19 04:31:19 +0800

    Three random number and calculate total

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -13,34 +13,105 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener{rollDice()}
-        val countButton : Button = findViewById(R.id.count_up)
-        countButton.setOnClickListener{countUp()}
+        rollButton.setOnClickListener { rollDice() }
+
+        val countButton: Button = findViewById(R.id.count_up)
+        countButton.setOnClickListener { countUp() }
+
         val resetButton: Button = findViewById(R.id.reset_button)
-        resetButton.setOnClickListener{resetBtn()}
+        resetButton.setOnClickListener { resetBtn() }
+
+        val calculateButton: Button = findViewById(R.id.calculate_button)
+        calculateButton.setOnClickListener { calculateTotal() }
+        calculateButton.isEnabled = false
     }
 
-    private fun rollDice(){
+    private fun rollDice() {
         //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-        val resultTest: TextView = findViewById(R.id.result_text)
-        val randomInt = Random().nextInt(6)+1
-        resultTest.text = randomInt.toString()
+
+        val resultTest1: TextView = findViewById(R.id.result_text)
+        val resultTest2: TextView = findViewById(R.id.result_text1)
+        val resultTest3: TextView = findViewById(R.id.result_text2)
+        val calculateButton : Button = findViewById(R.id.calculate_button)
+
+        val randomInt = Random().nextInt(6) + 1
+        val randomInt1 = Random().nextInt(6) + 1
+        val randomInt2 = Random().nextInt(6) + 1
+
+        resultTest1.text = randomInt.toString()
+        resultTest2.text = randomInt1.toString()
+        resultTest3.text = randomInt2.toString()
+
+        if(!calculateButton.isEnabled)
+        {
+            calculateButton.isEnabled = true
+        }
+
     }
 
-    private fun countUp(){
-        val resultTest : TextView = findViewById(R.id.result_text)
-        val resultNum = resultTest.text.toString()
+    private fun countUp() {
+        val resultTest1: TextView = findViewById(R.id.result_text)
+        val resultTest2: TextView = findViewById(R.id.result_text1)
+        val resultTest3: TextView = findViewById(R.id.result_text2)
+        val calculateButton : Button = findViewById(R.id.calculate_button)
 
-        if(resultNum == "Hello World!"){
-            resultTest.text = "1"
-        }else if (resultNum < "6") {
-            resultTest.text = (resultNum.toInt()+1).toString()
+        val resultNum1 = resultTest1.text.toString()
+        val resultNum2 = resultTest2.text.toString()
+        val resultNum3 = resultTest3.text.toString()
+
+        if(resultNum1 == "Random Number 1" || resultNum2 == "Random Number 2" || resultNum3 == "Random Number 3"){
+            if(resultNum1 == "Random Number 1"){
+                resultTest1.text = "1"
+            }
+            if(resultNum2 == "Random Number 2") {
+                resultTest2.text = "1"
+            }
+            if(resultNum3 == "Random Number 3"){
+                resultTest3.text = "1"
+            }
+        }else{
+            if(resultNum1 < "6") {
+                resultTest1.text = (resultNum1.toInt() + 1).toString()
+            }
+            if(resultNum2 < "6") {
+                resultTest2.text = (resultNum2.toInt() + 1).toString()
+
+            }
+            if(resultNum3 < "6") {
+                resultTest3.text = (resultNum3.toInt() + 1).toString()
+            }
+        }
+
+        if(!calculateButton.isEnabled)
+        {
+            calculateButton.isEnabled = true
         }
     }
 
-    private fun resetBtn(){
-        val resultTest : TextView = findViewById(R.id.result_text)
-        resultTest.text = "0"
+    private fun resetBtn() {
+        val resultTest1: TextView = findViewById(R.id.result_text)
+        val resultTest2: TextView = findViewById(R.id.result_text1)
+        val resultTest3: TextView = findViewById(R.id.result_text2)
+        val resultTotal: TextView = findViewById(R.id.result_total)
+
+        resultTest1.text = "0"
+        resultTest2.text = "0"
+        resultTest3.text = "0"
+        resultTotal.text = "0"
+    }
+
+    private fun calculateTotal() {
+        val resultTest1: TextView = findViewById(R.id.result_text)
+        val resultTest2: TextView = findViewById(R.id.result_text1)
+        val resultTest3: TextView = findViewById(R.id.result_text2)
+        val resultTotal: TextView = findViewById(R.id.result_total)
+
+        val resultNumb1 = resultTest1.text.toString().toInt()
+        val resultNumb2 = resultTest2.text.toString().toInt()
+        val resultNumb3 = resultTest3.text.toString().toInt()
+
+        resultTotal.text = (resultNumb1 + resultNumb2 + resultNumb3).toString()
     }
 }
