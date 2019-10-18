53517db843a2d145940e125df9f167511db0d76a
yspua1999 <56663047+yspua1999@users.noreply.github.com>
2019-10-19 02:28:01 +0800

    Done coding challenge

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +15,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{rollDice()}
+        val countButton : Button = findViewById(R.id.count_up)
+        countButton.setOnClickListener{countUp()}
+        val resetButton: Button = findViewById(R.id.reset_button)
+        resetButton.setOnClickListener{resetBtn()}
     }
 
     private fun rollDice(){
-        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        val resultTest: TextView = findViewById(R.id.result_text)
+        val randomInt = Random().nextInt(6)+1
+        resultTest.text = randomInt.toString()
+    }
+
+    private fun countUp(){
+        val resultTest : TextView = findViewById(R.id.result_text)
+        val resultNum = resultTest.text.toString()
+
+        if(resultNum == "Hello World!"){
+            resultTest.text = "1"
+        }else if (resultNum < "6") {
+            resultTest.text = (resultNum.toInt()+1).toString()
+        }
+    }
+
+    private fun resetBtn(){
+        val resultTest : TextView = findViewById(R.id.result_text)
+        resultTest.text = "0"
     }
 }
